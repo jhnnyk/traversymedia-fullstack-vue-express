6417679f6bf15c9b0e8e513bab0ae5b812bf6ccb
John Knotts <jhnnyk@gmail.com>
2019-10-29 16:53:35 -0600

    connect to the database

--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,12 +4,22 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // Get Posts
-router.get('/', (req, res) => {
-  res.send('hello');
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 // Add Post
 
 // Delete Post
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  return client.db('vue_express').collection('posts');
+}
 
 module.exports = router;
